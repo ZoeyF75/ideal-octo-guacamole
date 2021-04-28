18db45aa012387e1351567b2cfa74d7b3deab1bd
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-28 16:59:47 -0700

    Search component styling added

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Search = () => {
   return (
     <div className="search-container">
-      Hello
+      Movie Title
+      <div className="search">
+        <i class="fa fa-search"></i>
+        <textarea placeholder="Search for a movie title" required minLength="1"></textarea>
+      </div>
     </div>
   )
 }
