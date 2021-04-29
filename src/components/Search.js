85ff0b89bb573fd13e9f89150a7bb5fd34e85bf1
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-29 00:29:38 -0700

    Fixed enter issue

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 import Results from './Results';
-import Nominations from './Nominations'
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
+  const [text, setText] = useState('');
 
   const getMovies = async () => {
     const moviesFromServer = await fetchMovies();
@@ -23,13 +23,22 @@ const Search = () => {
     getMovies();
   }
 
+  window.onkeydown=function(event){
+    if(event.keyCode==13){
+      event.preventDefault();
+      setSearchTerm(event.target.value);
+      getMovies();
+    }
+  }
+
   return (
     <>
     <div className="search-container">
       <h5>Movie Title</h5>
+      <small>Press enter to complete search</small>
       <div className="search">
         <i className="fa fa-search"></i>
-        <textarea placeholder="Search for a movie title" required minLength="1" onChange={event => onChange(event)}></textarea>
+  <textarea placeholder="Search for a movie title" required minLength="1" onChange={event => onChange(event)}>{text}</textarea>
       </div>
     </div>
     <div className="results-nom-container">
